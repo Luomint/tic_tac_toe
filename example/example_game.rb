7a0435821a88fe6c09a8d819c082941c5eb0ac34
Karolis Laurinaitis <laurinaitis.karolis@gmail.com>
2015-07-17 09:44:03 +0100

    changed names in example

--- example/example_game.rb
+++ example/example_game.rb
@@ -2,7 +2,7 @@ root = File.expand_path("../", File.dirname(__FILE__))
 require "#{root}/lib/tic_tac_toe.rb"
 
 puts "Welcome to tic tac toe"
-bob = TicTacToe::Player.new({color: "X", name: "bob"})
-frank = TicTacToe::Player.new({color: "O", name: "frank"})
-players = [bob, frank]
+cat = TicTacToe::Player.new({color: "X", name: "cat"})
+dog = TicTacToe::Player.new({color: "O", name: "dog"})
+players = [cat, dog]
 TicTacToe::Game.new(players).play
